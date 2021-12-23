cb6c08d6d7f413c22c6809e8429a89e42985d396
Faizy <FaizyLhr>
2021-12-24 04:12:53 +0500

    comment api fixes

--- server/routes/api/comments.js
+++ server/routes/api/comments.js
@@ -2,14 +2,15 @@ let router = require("express").Router();
 
 let { OkResponse, BadRequestResponse } = require("express-http-response");
 
-const CommentsModel = require("../../models/Comments");
-const NewsModel = require("../../models/News");
+const Comments = require("../../models/Comments");
+const News = require("../../models/News");
 
-const { isAdmin, isUser, isToken, isCommentDel } = require("../auth");
+const { isAdmin, isUser, isToken } = require("../auth");
+const { isCommentDel } = require("../../middlewares/middlewares");
 
 // get Comment for every time commentSlug given
 router.param("commentSlug", (req, res, next, slug) => {
-	CommentsModel.findOne({ slug }, (err, comment) => {
+	Comments.findOne({ slug }, (err, comment) => {
 		if (!err && comment !== null) {
 			// console.log(comment);
 			req.comment = comment;
@@ -21,7 +22,7 @@ router.param("commentSlug", (req, res, next, slug) => {
 
 // get News for every time newsSlug given
 router.param("newsSlug", (req, res, next, slug) => {
-	NewsModel.findOne({ slug }, (err, news) => {
+	News.findOne({ slug }, (err, news) => {
 		if (!err && news !== null) {
 			// console.log(news);
 			req.news = news;
@@ -34,85 +35,60 @@ router.param("newsSlug", (req, res, next, slug) => {
 // Add Comment
 router.post("/add/:newsSlug", isToken, isAdmin, (req, res, next) => {
 	// console.log(!req.body.news);
+	console.log(req.news);
+	// console.log(!req.body.news);
 
 	// Validate User input
 	if (!req.body.text) {
 		return next(new BadRequestResponse("Missing required parameter", 422));
-	} else if (req.body.text.length === 0) {
+	} else if (req.body.text.trim().length === 0) {
 		return next(new BadRequestResponse("Missing required parameter", 422));
 	}
 
+	console.log("news");
 	// Create user in our database
-	let newComment = CommentsModel();
-	console.log(req.body);
+	let newComment = Comments();
 
 	if (req.body.text) {
 		newComment.text = req.body.text;
 	}
-
-	// console.log(newComment);
-
 	newComment.commentedBy = req.user._id;
 	newComment.commentedOn = req.news._id;
 
 	newComment.save((err, result) => {
-		if (err) {
-			// console.log(err);
-			return next(new BadRequestResponse(err));
-		} else {
-			req.news.comments.push(newComment._id);
-			req.news.save((err, data) => {
-				if (err) {
-					// console.log(err);
-					return next(new BadRequestResponse(err));
-				} else {
-					// console.log(data);
-					console.log("Id Added");
-				}
-			});
-			// console.log(result);
-			return next(new OkResponse(result.toJSON()));
-		}
+		if (err) return next(new BadRequestResponse(err));
+		req.news.comments.push(newComment._id);
+		req.news.save((err, data) => {
+			if (err) return next(new BadRequestResponse(err));
+			console.log("Id Added");
+		});
+		// console.log(result);
+		return next(new OkResponse(result));
 	});
 });
 
 // View All Comments
-router.get("/all/:newsSlug", isToken, (req, res, next) => {
+router.get("/get/all/:newsSlug", isToken, (req, res, next) => {
 	const options = {
 		page: +req.query.page || 1,
 		limit: +req.query.limit || 10,
-		populate: "commentedBy",
 	};
-	// console.log(req.news._id);
 
 	let query = {};
 	query.commentedOn = req.news._id;
 	query.isDeleted = false;
 
-	CommentsModel.paginate(query, options, function (err, result) {
-		if (err) {
-			// console.log(err);
-			return next(new BadRequestResponse("Server Error"), 500);
-		}
-		// console.log(result.docs);
-		result.docs = result.docs.map((news) => news.toJSON());
+	Comments.paginate(query, options, function (err, result) {
+		if (err) return next(new BadRequestResponse("Server Error"), 500);
 		return next(new OkResponse({ result: result.docs }));
-	}).catch((e) => {
-		// console.log(e);
-		return next(new BadRequestResponse(e.error));
 	});
 });
 
 // View Specific Comment
-router.get("/:commentSlug", isToken, (req, res, next) => {
-	console.log(req.comment);
-	if (req.comment) {
-		console.log("comment");
-
-		return next(new OkResponse(req.comment.toJSON()));
-	} else {
-		return next(new BadRequestResponse("comment not found!", 423));
-	}
+router.get("/get/:commentSlug", isToken, (req, res, next) => {
+	// console.log(req.comment);
+	if (req.comment) return next(new OkResponse(req.comment));
+	return next(new BadRequestResponse("comment not found!", 423));
 });
 
 //Change Status Delete a Comment
@@ -120,13 +96,8 @@ router.put("/del/:commentSlug", isToken, isCommentDel, (req, res, next) => {
 	// console.log(req.user);
 	req.comment.isDeleted = true;
 	req.comment.save((err, result) => {
-		if (err) {
-			// console.log(err);
-			return next(new BadRequestResponse(err));
-		} else {
-			// console.log(result);
-			return next(new OkResponse(req.comment.toJSON()));
-		}
+		if (err) return next(new BadRequestResponse(err));
+		return next(new OkResponse(req.comment));
 	});
 });
 
