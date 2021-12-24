10a6aab1386ff4f7bc85705c0bda9a2c839a0ef9
Faizy <FaizyLhr>
2021-12-24 15:48:40 +0500

    passport fixes

--- server/utilities/passport.js
+++ server/utilities/passport.js
@@ -5,14 +5,10 @@ let { BadRequestResponse, UnauthorizedResponse, ForbiddenResponse } = require("e
 
 const User = require("../models/User");
 
-const localStrategy = new LocalStrategy({ usernameField: "userName", passwordField: "password" }, (userName, password, done) => {
-	User.findOne({ userName }, (err, user) => {
-		if (err) {
-			return done(err);
-		}
-		if (!user) {
-			return done(null, false, { message: "Incorrect UserName Address" });
-		}
+const localStrategy = new LocalStrategy({ usernameField: "email", passwordField: "password" }, (email, password, done) => {
+	User.findOne({ email }, (err, user) => {
+		if (err) return done(err);
+		if (!user) return done(null, false, { message: "Incorrect UserName Address" });
 		if (user.validPassword(password) != true) {
 			return done(null, false, { message: "Incorrect Password" });
 		}
