8cf24dd25a65887e36c4f2bbf59119863c02c64b
Faizy <FaizyLhr>
2021-12-25 02:05:02 +0500

    Service Fixes

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './layout/header/header.component';
-
-
+import { SharedModule } from '../shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpTokenInterceptor } from './interceptors';
+import { ApiService, JwtService, NewsService, UserService } from './services';
+import { AuthGuard } from './services/auth-guard.service';
+import { NoAuthGuard } from './services/no-auth-guard.service';
 
 @NgModule({
-  declarations: [
-    LayoutComponent,
-    HeaderComponent
+  declarations: [LayoutComponent, HeaderComponent],
+  imports: [CommonModule, SharedModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
+    ApiService,
+    JwtService,
+    UserService,
+    AuthGuard,
+    NoAuthGuard,
+    NewsService,
   ],
-  imports: [
-    CommonModule
-  ]
 })
-export class CoreModule { }
+export class CoreModule {}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [],
@@ -12,7 +13,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ReactiveFormsModule,
     HttpClientModule,
     NgbModule,
+    RouterModule,
   ],
-  exports: [FormsModule, ReactiveFormsModule, HttpClientModule, NgbModule],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
+    RouterModule,
+  ],
 })
 export class SharedModule {}
