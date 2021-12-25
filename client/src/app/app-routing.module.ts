2f28394e305c23c4e2155e24c87b21d19a37deff
Faizy <FaizyLhr>
2021-12-26 00:16:07 +0500

    Upto Login Component Done

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoAuthGuard } from './core/services/no-auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./feature/auth/auth.module').then((m) => m.AuthModule),
+    // canActivate: [NoAuthGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
