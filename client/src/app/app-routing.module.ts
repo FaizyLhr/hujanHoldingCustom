cb6d52477a79a0366c45aca228c5373802930dba
Faizy <FaizyLhr>
2021-12-26 11:32:22 +0500

    Basic Layout Ready

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 import { NoAuthGuard } from './core/services/no-auth-guard.service';
 
 const routes: Routes = [
@@ -9,6 +10,31 @@ const routes: Routes = [
     loadChildren: () =>
       import('./feature/auth/auth.module').then((m) => m.AuthModule),
     // canActivate: [NoAuthGuard],
+  },
+  {
+    path: 'home',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./feature/home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'sponsor',
+        loadChildren: () =>
+          import('./feature/sponsor/sponsor.module').then(
+            (m) => m.SponsorModule
+          ),
+      },
+      {
+        path: 'community',
+        loadChildren: () =>
+          import('./feature/community/community.module').then(
+            (m) => m.CommunityModule
+          ),
+      },
+    ],
   },
 ];
 
